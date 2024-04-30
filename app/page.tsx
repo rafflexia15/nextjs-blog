313a7edba51f7f3dbd11e0974ad593a0c49cc85b
Ravenka <47773429+rafflexia15@users.noreply.github.com>
2024-04-30 12:39:55 +0700

    Update spotify.tsx
    updating playlist

--- app/page.tsx
+++ app/page.tsx
@@ -86,14 +86,7 @@ const Spotify1 = () => {
 const Spotify2 = () => {
   return (
     <div className="w-full md:w-[50%] flex items-center justify-center">
-      <iframe
-        src="https://open.spotify.com/embed/playlist/7mXsqwd2rPdYgY9lfQoV9l?utm_source=generator"
-        width="100%"
-        height="400"
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/42K0zKKOXsdhNBVomvacoT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   );
 };
